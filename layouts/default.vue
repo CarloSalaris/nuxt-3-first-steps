a2895b6a9a1408b0d6f49312fa7c1d6742faa622
CarloSalaris <carloa.salaris@gmail.com>
2023-10-06 11:15:00 +0200

    Index layout (ish)

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="vh-100 m-0 py-4" style="background-color: rgba(100, 169, 171, 0.726);">
-        <div class="container">
+        <div class="container h-100">
             <slot />
         </div>
     </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,11 @@
 <template>
-    <div class="text-center">
-        <h1>Hello, World!</h1>
-        <NuxtLink class="btn btn-primary"
-            to="/course/chapter/1-chapter-1/lesson/1-introduction-to-typescript-with-vue-js-3">
-            VAI ALLE LEZIONI!
-        </NuxtLink>
+    <div class="my-5 d-flex text-center justify-content-center">
+        <div class="mt-5 p-5">
+            <h1 class="mb-4">Hello, World!</h1>
+            <NuxtLink class="btn btn-primary"
+                to="/course/chapter/1-chapter-1/lesson/1-introduction-to-typescript-with-vue-js-3">
+                VAI ALLE LEZIONI!
+            </NuxtLink>
+        </div>
     </div>
 </template>
